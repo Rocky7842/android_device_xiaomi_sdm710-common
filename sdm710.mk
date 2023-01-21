3140c421b538df50e0a5e6870c81db5713aa1ab7
me-cafebabe <me_cafebabe@hotmail.com>
2023-01-21 15:36:03 +0800

    sdm710-common: Switch to QTI Vibrator AIDL
    It provides haptic effects with qti haptics driver,
    Meanwhile still supports simple vibration with qpnp haptics driver.
    
    Change-Id: I83d0c5f562c855afba563e7b959d3e0cd697ef26

--- sdm710.mk
+++ sdm710.mk
@@ -403,8 +403,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+    vendor.qti.hardware.vibrator.service
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
