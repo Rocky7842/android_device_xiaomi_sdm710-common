5616d051b98f86da2e6e3dd43a47de30f26f0bcf
Rocky7842 <49489159+Rocky7842@users.noreply.github.com>
2023-02-09 19:30:17 +0800

    sdm710-common: Add power hal extension to deal with DT2W

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -101,7 +101,7 @@ BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := sdm710
 
 # Power
-TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
+TARGET_POWERHAL_MODE_EXT := $(COMMON_PATH)/power/power-mode.cpp
 
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
