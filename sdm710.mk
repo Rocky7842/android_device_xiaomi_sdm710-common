87b373bb410ffa7b704680fd1a925e15442781fc
Rocky7842 <49489159+Rocky7842@users.noreply.github.com>
2023-05-03 22:42:30 +0800

    sdm710-common: Upver soundtrigger hal to 2.3

--- sdm710.mk
+++ sdm710.mk
@@ -58,7 +58,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl \
-    android.hardware.soundtrigger@2.1-impl
+    android.hardware.soundtrigger@2.3-impl
 
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
