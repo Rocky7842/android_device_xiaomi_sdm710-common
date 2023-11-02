fa50b990dc9c76fb7d9b0175239b2c271563a679
Rocky7842 <eric.rocky7842@gmail.com>
2023-11-02 22:12:43 +0800

    sdm710-common: Downrev to v7.0 audio HAL
    * v7.1 Breaks boot on U due to unimplemented StreamOut::setLatencyMode.

--- sdm710.mk
+++ sdm710.mk
@@ -54,7 +54,7 @@ PRODUCT_COMPRESSED_APEX := false
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@7.1-impl \
+    android.hardware.audio@7.0-impl \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl \
