eee726c2db419c129c8ff4b4f651b3ee84feca8d
Rocky7842 <eric.rocky7842@gmail.com>
2023-12-17 18:49:14 +0800

    sdm710-common: Decommonize libstdc++
    * Only sirius needs it.

--- sdm710.mk
+++ sdm710.mk
@@ -105,7 +105,6 @@ PRODUCT_PACKAGES += \
     libdng_sdk.vendor \
     libgui_vendor \
     libmegface \
-    libstdc++_vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
