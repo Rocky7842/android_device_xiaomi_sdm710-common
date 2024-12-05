d2d7987bcdfe2a71aa68e48ff175411b77b853c1
LuK1337 <priv.luk@gmail.com>
2024-12-05 16:41:51 +0100

    sdm710-common: Build libqti_vndfwk_detect.vendor
    Before:
    audio_hw_extn: spkr_prot_feature_init: Called with feature Enabled,
    vendor_enhanced_info 0x0
    
    After:
    audio_hw_extn: spkr_prot_feature_init: Called with feature Enabled,
    vendor_enhanced_info 0x3
    
    Change-Id: Ie5c33eafc1e0be2eeda8e0a6bbbc3712e32986e9

--- sdm710.mk
+++ sdm710.mk
@@ -274,6 +274,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
+# QTI fwk-detect
+PRODUCT_PACKAGES += \
+    libqti_vndfwk_detect.vendor
+
 # Rootdir
 PRODUCT_PACKAGES += \
     fstab.qcom \
