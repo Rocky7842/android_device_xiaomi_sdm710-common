9292a5d92ac29e861e5913182d8be41b13b60793
Ivan Vecera <ivan@cera.cz>
2024-12-12 16:30:39 +0100

    sdm710-common: Switch to py extract utils
    Change-Id: I10a2bc3472ff3294823a4565f80b558c6c14d9ec

---
+++ extract-files.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'device/xiaomi/sdm710-common',
+]
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    'system_ext/lib64/lib-imsvideocodec.so': blob_fixup()
+        .add_needed('libgui_shim.so'),
+    'vendor/etc/seccomp_policy/vendor.qti.hardware.dsp.policy': blob_fixup()
+        .add_line_if_missing('madvise: 1'),
+    'vendor/lib64/libwvhidl.so': blob_fixup()
+        .add_needed('libcrypto_shim.so'),
+    'vendor/lib64/mediadrm/libwvdrmengine.so': blob_fixup()
+        .add_needed('libcrypto_shim.so'),
+    (
+        'vendor/etc/data/dsi_config.xml',
+        'vendor/etc/data/netmgr_config.xml',
+    ): blob_fixup().fix_xml(),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'sdm710-common',
+    'xiaomi',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+)
+
+module.add_proprietary_file('proprietary-files-nfc.txt')
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
