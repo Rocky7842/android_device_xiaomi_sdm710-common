e21caca4967985fb95baa8b036b78cca61d1288c
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2025-05-06 15:24:50 +0545

    sdm710-common: switch to common AIDL IR service
    Change-Id: I36c05aaa312969e479896c6e63a26a68cf897afa

--- sdm710.mk
+++ sdm710.mk
@@ -100,7 +100,7 @@ PRODUCT_PACKAGES += \
 
 # Consumer IR
 PRODUCT_PACKAGES += \
-    android.hardware.ir-service.xiaomi
+    android.hardware.ir-service.lineage
 
 # Device-specific settings
 PRODUCT_PACKAGES += \
