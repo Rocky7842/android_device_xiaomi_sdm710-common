bf43afbb042364b60ba0b5b1a284a840eba638f9
Rocky7842 <eric.rocky7842@gmail.com>
2025-08-26 21:15:22 +0800

    sdm710-common: Build Dolby audio
    Change-Id: Idb6a4538697d936c48c6adea18769294f19dfc57

--- sdm710.mk
+++ sdm710.mk
@@ -89,6 +89,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
+# Audio (Dolby)
+$(call inherit-product-if-exists, hardware/dolby/dolby.mk)
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
