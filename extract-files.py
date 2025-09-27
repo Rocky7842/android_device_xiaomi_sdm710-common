f2490e610e7bf707dd1b0a1b09a4900af8aa6e0a
Rocky7842 <eric.rocky7842@gmail.com>
2025-09-27 17:11:18 +0800

    sdm710-common: Drop QCOM WFD
    * Newer QCOM WFD stack seems to rely on newer gralloc, which we don't
      support. While older ones simply won't work on newer android
      versions. Drop QCOM WFD and use AOSP implementation instead.
    
    Change-Id: I3cb290cda857e43ad409498187fea24d849d765a

--- extract-files.py
+++ extract-files.py
@@ -64,8 +64,6 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
-    'system_ext/lib/libwfdservice.so': blob_fixup()
-        .replace_needed('android.media.audio.common.types-V2-cpp.so','android.media.audio.common.types-V4-cpp.so'),
     'system_ext/lib64/lib-imsvideocodec.so': blob_fixup()
         .add_needed('libgui_shim.so'),
     'vendor/etc/seccomp_policy/atfwd@2.0.policy': blob_fixup()
