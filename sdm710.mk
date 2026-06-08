602dd4eaaaf046a19da8cf49920a08c8fb02faa6
Rocky7842 <eric.rocky7842@gmail.com>
2026-06-08 10:44:24 +0800

    sdm710-common: Implement torch strength control
    Change-Id: Ica93f3795c1d4516436341572267c33439bfeb31

--- sdm710.mk
+++ sdm710.mk
@@ -94,6 +94,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service.sdm710
 
+$(call soong_config_set,libcameraservice,ext_lib,//$(LOCAL_PATH):libcameraservice_extension.sdm710)
+
 # Configstore
 PRODUCT_PACKAGES += \
     disable_configstore
